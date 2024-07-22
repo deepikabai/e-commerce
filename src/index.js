2ec45d42c0a1bf92d110647b84f17288cae898d7
deepikabai <deepikamahadevan01@gmail.com>
2024-07-23 00:24:40 +0530

    Ready to deploy

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Redux/Store';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (Process.env.NODE_ENV === 'production') disableReactDevTools()
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+      
+  
+      
+    </BrowserRouter>
+    
+  </React.StrictMode>
+);
+
+reportWebVitals();
